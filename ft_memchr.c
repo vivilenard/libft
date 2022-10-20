b1e84a097181500733158809c91d055e603de563
Vivien Lenard <vlenard@1-E-8.42heilbronn.de>
2022-10-20 15:09:54 +0200

    norm

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,22 +6,24 @@
 /*   By: vlenard <vlenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 15:37:13 by vlenard           #+#    #+#             */
-/*   Updated: 2022/10/18 15:51:43 by vlenard          ###   ########.fr       */
+/*   Updated: 2022/10/20 15:09:02 by vlenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-const void *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t i;
-	i = 0;
+	size_t	i;
+	char	ci;
 
+	ci = c;
+	i = 0;
 	while (i < n)
 	{	
-		if (((unsigned char *) s)[i] == c)
-			return(&s[i]);
+		if (((unsigned char *) s)[i] == ci)
+			return ((void *) s + i);
 		i++;
 	}
-	return(NULL);
+	return (NULL);
 }
