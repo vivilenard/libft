4463c7e170a7757b777bccd3af7952d6d8ff1be1
Vivien Lenard <vlenard@1-C-3.42heilbronn.de>
2022-10-21 07:17:25 +0200

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: vlenard <vlenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 09:43:00 by vlenard           #+#    #+#             */
-/*   Updated: 2022/10/18 09:55:21 by vlenard          ###   ########.fr       */
+/*   Updated: 2022/10/20 14:49:53 by vlenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,16 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	unsigned int	i;
 
 	i = 0;
-	while (s1[i] == s2[i] && (s1[i] != '\0' && s2[i] != '\0'))
+	while (i != n)
 	{
-		if (i == (n - 1))
-			break ;
+		if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+		else if (s1[i] < s2[i])
+			return (- (s2[i] - s1[i]));
 		i++;
 	}
-	if (s1[i] > s2[i])
-		return (s1[i] - s2[i]);
-	else if (s1[i] < s2[i])
-		return (s1[i] - s2[i]);
-	else if (s1[i] == s2[i])
-		return (0);
 	return (0);
 }
