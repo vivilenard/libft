2f05f5bf6fae58523182fff15aa0d4fd56539b4d
Vivien Lenard <vlenard@1-C-3.42heilbronn.de>
2022-10-21 14:31:03 +0200

    iji

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: vlenard <vlenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 10:03:54 by vlenard           #+#    #+#             */
-/*   Updated: 2022/10/21 11:30:39 by vlenard          ###   ########.fr       */
+/*   Updated: 2022/10/21 14:30:02 by vlenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	size_t	i;
-	unsigned char uc;
+	size_t			i;
+	unsigned char	uc;
 
 	uc = (unsigned char) c;
 	i = 0;
 	while (s[i])
-	{ 
+	{
 		if (s[i] == uc)
 			return ((char *)s + i);
 		i++;
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,34 +6,31 @@
 /*   By: vlenard <vlenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 10:20:12 by vlenard           #+#    #+#             */
-/*   Updated: 2022/10/18 10:54:37 by vlenard          ###   ########.fr       */
+/*   Updated: 2022/10/21 14:30:34 by vlenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-const char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	n;
 
 	i = 0;
-	if (ft_strlen(needle) == 0)
-		return (haystack);
+	if (ft_strlen(needle) == '\0')
+		return ((char *)haystack);
 	while (haystack[i] != '\0')
 	{
-		if (i + 1 == len)
-			break ;
-		if (haystack[i] == needle[0])
+		n = 0;
+		while (haystack[i + n] == needle[0 + n]
+			&& (haystack[i + n] != '\0') && i < len)
 		{
-			n = 0;
-			while (haystack[i + n] == needle[0 + n]
-				&& (haystack[i + n] != '\0' && needle[0 + n] != '\0'))
-			{
-				if (needle[0 + n + 1] == '\0')
-					return (&haystack[i]);
-				n++;
-			}
+			if (needle[0 + n + 1] == '\0')
+				return (((char *)haystack + i));
+			n++;
+			if ((i + n) == len)
+				return (NULL);
 		}
 		i++;
 	}
