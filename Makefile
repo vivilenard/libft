a8634d90d842ea389f4525d6eadadaaa4af9332a
Vivien Lenard <vlenard@1-A-30.42heilbronn.de>
2022-12-22 13:10:12 +0100

    added printf and utodouble

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: vlenard <vlenard@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/15 15:04:49 by vlenard           #+#    #+#              #
-#    Updated: 2022/11/07 12:05:18 by vlenard          ###   ########.fr        #
+#    Updated: 2022/12/21 15:10:22 by vlenard          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,7 +17,8 @@ SRC = 	ft_isdigit.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isprint.c \
 		ft_strnstr.c ft_bzero.c ft_memset.c ft_memcpy.c ft_memchr.c ft_memcmp.c \
 		ft_memmove.c ft_atoi.c ft_strchr.c ft_strrchr.c ft_calloc.c ft_strdup.c \
 		ft_substr.c ft_strjoin.c ft_strtrim.c ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c \
-		ft_putnbr_fd.c ft_itoa.c ft_strmapi.c ft_striteri.c ft_split.c
+		ft_putnbr_fd.c ft_itoa.c ft_strmapi.c ft_striteri.c ft_split.c ft_isXxps.c \
+		ft_printf.c ft_takearg.c ft_tohex.c ft_topointer.c ft_utoa.c ft_strcmp.c ft_atodouble.c
 SRCBONUS = ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c ft_lstadd_back.c ft_lstdelone.c \
 		ft_lstclear.c ft_lstiter.c ft_lstmap.c
 OBJS = $(SRC:.c=.o)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vlenard <vlenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 16:59:12 by vlenard           #+#    #+#             */
-/*   Updated: 2022/11/07 17:10:20 by vlenard          ###   ########.fr       */
+/*   Updated: 2022/12/21 15:10:52 by vlenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,15 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdarg.h>
 
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }				t_list;
+
+typedef unsigned long int	t_intptr;
 
 int			ft_isalnum(int c);
 int			ft_isalpha(int c);
@@ -34,6 +37,7 @@ char		*ft_strrchr(const char *s, int c);
 size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t		ft_strlen(const char *s);
+int			ft_strcmp(const char *s1, const char *s2);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
 char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
 void		ft_bzero(void *s, size_t n);
@@ -43,7 +47,7 @@ void		*ft_memchr(const void *s, int c, size_t n);
 void		*ft_memmove(void *dst, const void *src, size_t len);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 int			ft_atoi(const char *str);
-void		*ft_calloc(size_t nmemb, size_t size);
+void		*ft_calloc(size_t count, size_t size);
 char		*ft_strdup(const char *s);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
@@ -65,4 +69,15 @@ void		ft_lstdelone(t_list *lst, void (*del)(void *));
 void		ft_lstclear(t_list **lst, void (*del)(void *));
 void		ft_lstiter(t_list *lst, void (*f)(void *));
 t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+int			ft_printf(const char *s, ...);
+char		*ft_utoa(unsigned int n);
+char		*ft_tohex(unsigned int d, char c);
+int			ft_takearg(const char *s, int i, va_list args);
+char		*ft_ultopointer(t_intptr n);
+int			ft_isbigx(va_list args, int print_length);
+int			ft_isx(va_list args, int print_length);
+int			ft_ispointer(va_list args, int print_length);
+int			ft_iss(va_list args, int print_length);
+int			ft_putandcountstr(char *str, int print_length);
+double		ft_atodouble(char *str);
 #endif
